f3a27968fa0d987a4dc62db99265daab767e1bba
clvazquez23 <clvazquez23@gmail.com>
2020-08-27 14:59:02 -0500

    Went back to my original JS

--- src/index.js
+++ src/index.js
@@ -25,20 +25,31 @@ if (minutes < 10) {
 
 dateTime.innerHTML = `${day}, ${hours}:${minutes}`;
 
+function search(event) {
+  event.preventDefault();
+  let searchbar = document.querySelector("#city-search");
+  let cityChange = document.querySelector("#card-city");
+  if (searchbar.value) {
+    cityChange.innerHTML = `${searchbar.value}`;
+  } else {
+    cityChange.innerHTML = null;
+    alert("Please enter your city");
+  }
+}
+
 let form = document.querySelector(".searchbar");
 form.addEventListener("submit", search);
 
-function displayWeatherCondition(response) {
-  document.querySelector("#location").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = 80;
+}
 
-
-function searchCity(city) {
-  let apiKey = "2b9d73225ef9671dd10e582640edb6d6";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-  axios.get(`${url}&appid=${apiKey}`).then(showTemperature);
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = 18;
 }
 
 function showTemperature(response) {
@@ -47,6 +58,12 @@ function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let weatherDiv = document.querySelector(".temperature");
   weatherDiv.innerHTML = `${temperature}ºC`;
+}
+
+function searchCity(city) {
+  let apiKey = "2b9d73225ef9671dd10e582640edb6d6";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${url}&appid=${apiKey}`).then(showTemperature);
 }
 
 searchCity("Austin");
